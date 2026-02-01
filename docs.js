74c29a320293ead0609d5b5434ead31f6b625b82
nvadnala6 <nvadnala@yahoo.com>
2026-02-01 17:11:26 -0500

    Update docs.js

--- docs.js
+++ docs.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
-import { db } from '../db.js';
+import { db } from './db.js';
 import { requireAuth, requireRole } from '../middleware/auth.js';
 
 const router = Router();
