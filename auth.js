7526ef620e84871a2d32df634acb8340ec093549
nvadnala6 <nvadnala@yahoo.com>
2026-02-01 17:09:57 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { db } from '../db.js';
+import { db } from './db.js';
 
 const router = Router();
 
